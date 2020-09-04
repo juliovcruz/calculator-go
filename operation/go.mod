82576e0a5b5d9a24c0012ffe645dd369f0b132f6
Julio V. Cruz <juliovcruz0@gmail.com>
2020-09-04 12:59:51 -0300

    feat: should main connect in grpc server

--- operation/go.mod
+++ operation/go.mod
@@ -3,7 +3,8 @@ module operation
 go 1.14
 
 require (
-	github.com/golang/protobuf v1.4.2 // indirect
-	github.com/joho/godotenv v1.3.0 // indirect
-	google.golang.org/grpc v1.31.1 // indirect
+	github.com/golang/protobuf v1.4.2
+	github.com/joho/godotenv v1.3.0
+	github.com/stretchr/testify v1.6.1
+	google.golang.org/grpc v1.31.1
 )
---
+++ operation/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"operation/proto"
+	"os"
+
+	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
+)
+
+var TEST_PORT string
+var SERVER_PORT string
+
+type OperationServiceServer struct{}
+
+func (s *OperationServiceServer) Division(ctx context.Context, req *proto.DivisionRequest) (*proto.DivisionResponse, error) {
+	return &proto.DivisionResponse{Result: 1}, nil
+}
+
+func (s *OperationServiceServer) Multiplication(ctx context.Context, req *proto.MultiplicationRequest) (*proto.MultiplicationResponse, error) {
+	return &proto.MultiplicationResponse{Result: 1}, nil
+}
+
+func (s *OperationServiceServer) Sum(ctx context.Context, req *proto.SumRequest) (*proto.SumResponse, error) {
+	return &proto.SumResponse{Result: 1}, nil
+}
+
+func (s *OperationServiceServer) Subtraction(ctx context.Context, req *proto.SubtractionRequest) (*proto.SubtractionResponse, error) {
+	return &proto.SubtractionResponse{Result: 1}, nil
+}
+
+func main() {
+	err := readEnv()
+	if err != nil {
+		log.Fatalf("Error in read env: %v", err)
+	}
+
+	listener, err := net.Listen("tcp", ":"+SERVER_PORT)
+	if err != nil {
+		log.Fatalf("Error in listen port:" + SERVER_PORT)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	proto.RegisterOperationServiceServer(grpcServer, &OperationServiceServer{})
+
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Error in connection gRpc with listener")
+	}
+
+}
+
+func readEnv() error {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		return err
+	}
+	TEST_PORT = os.Getenv("TEST_PORT")
+	SERVER_PORT = os.Getenv("SERVER_PORT")
+	return nil
+}
