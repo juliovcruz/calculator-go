d79f3b1608d84f1f85596d37ac2c2b6618e18204
Julio V. Cruz <juliovcruz0@gmail.com>
2020-09-10 16:36:56 -0300

    test: add division tests

--- operation/server/server_test.go
+++ operation/server/server_test.go
@@ -47,3 +47,42 @@ func TestSum(t *testing.T) {
 	)
 
 }
+
+func TestDivision(t *testing.T) {
+	s := OperationServiceServer{}
+
+	mapTest := map[float64]OperationModel{
+		20: OperationModel{
+			number1: 40,
+			number2: 2,
+		},
+		0: OperationModel{
+			number1: 5,
+			number2: 0,
+		},
+		30: OperationModel{
+			number1: 60,
+			number2: 2,
+		},
+	}
+
+	t.Run("Division",
+		func(t *testing.T) {
+			for key, value := range mapTest {
+				res, err := s.Division(context.Background(),
+					&proto.DivisionRequest{
+						Operation: &proto.Operation{
+							Number1: value.number1,
+							Number2: value.number2,
+						},
+					},
+				)
+				if err != nil {
+					t.Fatalf("Error in execution method %v:", err)
+				}
+				assert.Equal(t, key, res.Result)
+			}
+		},
+	)
+
+}
